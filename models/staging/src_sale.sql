a924e7a5bed3666907bafc28b96429891af356bc
surendra singh <surendra.yadav555@gmail.com>
2025-10-15 19:24:57 +0000

    commit all changes

--- models/staging/src_sale.sql
+++ models/staging/src_sale.sql
@@ -1,1 +1,5 @@
-select * from {{ source('staging','sale') }}
+with source as (select * from {{ source("staging", "sale") }})
+
+select cast(sale_id as int) as sale_id, sale_date, amount
+from source
+where sale_id is not null
